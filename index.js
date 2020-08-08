35b0a98f611d4634ae269c1fc8c0d9bd87e47aef
tomerk97 <69254009+tomerk97@users.noreply.github.com>
2020-08-09 02:44:34 +0300

    FirstUpload

---
+++ index.js
@@ -0,0 +1,32 @@
+// let series = [];
+var Save = document.getElementById("saveinput");
+
+// let CheckifSeen = function(name){
+// 	for(let i=0;i<series.length;i++)
+// 		{
+// 			if(name === series[i].Name)
+// 			{
+// 				return Number(i);
+// 			}
+			
+// 		}
+// 		return false;
+// }
+// let addNew = function(NewName, NewSeason,NewEpisode)
+// {
+// 		if(!CheckifSeen(NewName))
+// 		{
+// 		series.push({Name:NewName,Season:NewSeason,Episode:NewEpisode});
+// 		}
+// 		else
+// 		{
+// 			let place = CheckifSeen(NewName);
+// 			series.splice(place,1,{Name:NewName,Season:NewSeason,Episode:NewEpisode});
+			
+// 		}
+// }
+alert("hey");
+Save.addEventListener("click", alert("hey");
+let displayArray = function(){
+
+}
